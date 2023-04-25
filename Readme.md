8a7a74533155151f4ed12d0a6ee331ec53976e7f
eddysantiagoo <eddysantiagogh@gmail.com>
2023-04-25 10:17:16 -0500

    :tada: Initial commit

---
+++ Readme.md
@@ -0,0 +1,64 @@
+# SVG Spotify widget 🎶
+
+
+## Set up 👀
+
+* You will need to make a Spotify Application first To do so visit [Spotify Developer portal](https://developer.spotify.com/dashboard/applications)  
+* Login and then hit create App
+
+![](./assets/create.png)
+
+* Fill out the name and the Description and click create.
+
+* Click Edit Settings and add `http://127.0.0.1:5000/spotify` to the redirect URIs  
+(P.s don't forget to scroll down and click save)
+
+* Now Copy the Client ID and The Client Secret
+
+![](./assets/creds.png)
+
+* Fork this Repo
+
+* Clone the Github Repo
+
+``` git clone https://github.com/Om-Thorat/Readme-Spotify-Now-Playing.git```
+
+* Now move into the folder
+
+```cd Readme-Spotify-Now-Playing```
+
+* Install the dependencies
+
+```pip install -r requirements.txt```
+
+* Make a tmp directory 
+
+```mkdir tmp```
+
+* Now paste the Credentials you copied into their respective places in your main.py file
+
+( ⚠️ make sure not to commit this repo with this sensitive info)
+
+* Run the script and a token.json file is created Copy the "refresh_token" from the file.
+
+### Now that we have all our ids secrets and tokens let's create the app 👀
+
+* Create a new vercel app with the repo your forked linked to it.
+
+* Add the following environment variables while creating the app 
+> don't worry if you forget you can always edit them in settings > Environment variables
+
+* CLIENT_ID = client id copied from spotify dashboard  
+* CLIENT_SECRET = client secret copied from spotify dashboard
+* REFRESH = refresh token copied from json file
+
+Click save and **voila!** you have your own svg spotify widget 🎉
+
+Visit ```{yoururl}.vercel.app/spotify``` to see 👀
+
+To use it in your readme's just link to your hosting url /spotify  
+for example:
+```![](https://{yoururl}.vercel.app/spotify)```
+
+Thanks,
+💖 Happy Coding.
---
+++ api/index.py
@@ -0,0 +1,89 @@
+from flask import Flask,Response,request,redirect,render_template
+import spotipy
+from spotipy import util
+from pathlib import Path
+import re
+import base64
+from PIL import Image
+import requests
+from io import BytesIO
+import random
+from spotipy import oauth2
+import os
+
+
+
+cwd = Path.cwd()
+cwd = re.sub(r"\\",r"/",str(cwd))
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+scope = "user-read-playback-state user-read-recently-played"
+redirect_uri = "http://127.0.0.1:5000/spotify"
+
+app = Flask(__name__)
+try:
+    Refresh = os.environ['REFRESH']
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": Refresh,
+        }
+
+    res = requests.post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "refresh_token",
+            "refresh_token": Refresh,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    )
+    token = res.json()['access_token']
+except:
+    token = util.prompt_for_user_token("",scope, CLIENT_ID, CLIENT_SECRET, redirect_uri,cache_path=f"token.json")
+sp = spotipy.Spotify(auth=token)
+def spotify():
+    try:
+        current = (sp.current_playback())
+        songname = (current['item']['name'])
+        artist = (current['item']['artists'][0]['name'])
+        cover = (current['item']['album']['images'][0]['url'])
+        return ["Now Playing",songname,cover,artist]
+    except:
+        current = sp.current_user_recently_played(limit=1)
+        songname = (current['items'][0]['track']['name'])
+        artist = (current['items'][0]['track']['artists'][0]['name'])
+        cover = (current['items'][0]['track']['album']['images'][0]['url'])
+        return ["last played",songname,cover,artist]
+    
+@app.route('/')
+def hi():
+     return "Nothing her, go to /spotify :p"
+
+@app.route('/spotify')
+def cool():
+        info = spotify()
+        print(info)
+        songname = info[1].replace("&","&amp;")
+        artist = info[3].replace("&","&amp;")
+        response = requests.get(info[2])
+        img = Image.open(BytesIO(response.content))
+        img.save(f"/tmp/haha.jpg",format="JPEG")
+        with open(f"/tmp/haha.jpg","rb") as img_file:
+            imgstr = base64.b64encode(img_file.read()).decode()
+        bars = ""
+        for i in range(0,32):
+            bars = bars + (f'<div class="bar" style="animation-delay: {random.randint(500,800)}ms; animation-duration: {random.randint(800,1000)}ms"></div>')
+        data = {
+            "status" : info[0],
+            "songname":songname,
+            "artist":artist,
+            "imgstr":imgstr,
+            "bars":bars
+        }
+        aight = render_template("card.html.j2",**data)
+        resp = Response(aight,mimetype='image/svg+xml')
+        resp.headers['Cache-Control'] = 'public, max-age=0, must-revalidate'
+        return resp
+
+
+# app.run(debug=True)
